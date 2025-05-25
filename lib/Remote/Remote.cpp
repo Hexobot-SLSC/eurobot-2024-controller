7d2f1d78128e62f677c73cb31492c9f556f75205
zoykad <zoykad912@gmail.com>
2025-05-25 21:44:43 +0200

    Add SecretSettings.h template and update Remote.cpp to use defined constants

--- lib/Remote/Remote.cpp
+++ lib/Remote/Remote.cpp
@@ -5,20 +5,19 @@
 #include <RF24.h>
 #include <nRF24L01.h>
 #include <SPI.h>
+#include <SecretSettings.h>
 
 Remote::Remote(): radio(RADIO_CE, RADIO_CSN) {};
 
 bool Remote::setup() {
-    const byte writingAddress[6] = "912CR";
     bool result;
 
     result = radio.begin();
     
-    radio.setChannel(110);
+    radio.openWritingPipe((const byte*)RF_ADDRESS); // address
+    radio.setChannel(RADIO_CHANNEL);
     radio.setPALevel(RF24_PA_LOW);
     radio.setDataRate(RF24_2MBPS);
-
-    radio.openWritingPipe(writingAddress); // address
 
     radio.stopListening();
 
---
+++ template/SecretSettings.h
@@ -0,0 +1,7 @@
+// Put this file in the src directory of your project to define secret settings.
+// The project won't compile without this file.
+
+#pragma once
+
+#define RF_ADDRESS "XXXXX"
+#define RADIO_CHANNEL 100
